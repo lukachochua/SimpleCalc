bc80d1c66a79702c5ffd98da20fbef4a87445fcb
lukachochua <lukachochua@gmail.com>
2024-07-08 15:16:26 +0400

    feat: add custom validation for the program not to accept invalid inputs

--- Program.cs
+++ Program.cs
@@ -8,43 +8,85 @@ class Program
 {
     static void Main(string[] args)
     {
-        int num1;
-        int num2;
+        int num1 = 0;
+        int num2 = 0;
         int result = 0;
 
         string answer;
         Console.WriteLine("Hello, Welcome to the simple Calculator!");
-        Console.WriteLine("Enter the First Number");
-        num1 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Enter the Second Number");
+        // Input for num1
+        bool validInput = false;
+        while (!validInput)
+        {
+            Console.WriteLine("Enter the First Number");
+            try
+            {
+                num1 = Convert.ToInt32(Console.ReadLine());
+                validInput = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter an integer.");
+            }
+        }
 
-        num2 = Convert.ToInt32(Console.ReadLine());
+        // Input for num2
+        validInput = false;
+        while (!validInput)
+        {
+            Console.WriteLine("Enter the Second Number");
+            try
+            {
+                num2 = Convert.ToInt32(Console.ReadLine());
+                validInput = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter an integer.");
+            }
+        }
 
         Console.WriteLine("Which Math Operation Would You Like To Do - \"+, -, *, /\" ?");
-        Console.WriteLine("");
 
         answer = Console.ReadLine();
 
-        if (answer.Equals("+"))
+        bool inputValidation = false;
+
+        while (!inputValidation)
         {
-            result = num1 + num2;
-        }
-        else if (answer.Equals("-"))
-        {
-            result = num1 - num2;
-        }
-        else if (answer.Equals("*"))
-        {
-            result = num1 * num2;
-        }
-        else if (answer.Equals("/"))
-        {
-            result = num1 / num2;
-        }
-        else
-        {
-            Console.WriteLine("Incorrect math operator. Try again.");
+            Console.WriteLine("Which Math Operation Would You Like To Do - \"+, -, *, /\" ?");
+            answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "+":
+                    result = num1 + num2;
+                    inputValidation = true;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    inputValidation = true;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    inputValidation = true;
+                    break;
+                case "/":
+                    try
+                    {
+                        result = num1 / num2;
+                        inputValidation = true;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Incorrect math operator. Try again.");
+                    break;
+            }
         }
 
         Console.WriteLine($"The result is {result}");
--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637484c9a9f823efe7d0573e3178f0d5e6ce8dcb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9855a99a047561deb224eb6ca71371acd95822d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
